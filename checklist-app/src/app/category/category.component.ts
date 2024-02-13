e3ec88faec82d6afc1d3da5a57dabd4e5e4b22a8
Marcelo P. Santos <gcontatomarcelo@gmail.com>
2024-02-12 23:54:05 -0300

    correção da tela e do funcionamento do Dialog.

--- checklist-app/src/app/category/category.component.ts
+++ checklist-app/src/app/category/category.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
 import { Category } from '../_models/category';
-import { DialogComponent } from '../dialog/dialog.component';
 import { MaterialModule } from '../material.module';
+import { DialogComponent } from './../dialog/dialog.component';
 
 const CATEGORY_DATA =[
   {name: 'Educação', guid: 'aaa-bbb-ccc-ddd'},
@@ -20,27 +20,29 @@ const CATEGORY_DATA =[
 })
 export class CategoryComponent {
 
-  constructor(private dialog: MatDialog){
-
-
-  }
+  constructor(private dialog: MatDialog){}
 
   displayedColumns: string[] = ['id', 'name', 'actions'];
   dataSource: Category[]=CATEGORY_DATA;
 
   public editCategory(category: Category) {
-
     console.log('Edit new category clicked')
   }
+
   public deleteCategory(category: Category) {
-    this.dialog.open(DialogComponent, {disabledClose: true, }).afterClosed().subscribe(
-    resp => {
-      if(resp) console.log('Categoria apagada com sucesso!');
-    }else{
-      if(resp) console.log('Categoria não apagada');
-    }
-  )
-}
+
+    this.dialog.open(DialogComponent, {disableClose: true,
+      data: {dialogMsg:'Você tem certeza que gostaria de apagar a categoria?', leftButtonLabel:'Cancelar', rightButtonLabel:'Sim'}}).afterClosed().subscribe(
+      resp => {
+        if(resp){
+          console.log('Categoria apagada com sucesso!');
+      }else{
+          console.log('Categoria não apagada!');
+        }
+      }
+    )
+
+  }
 
 
 
